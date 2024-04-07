7bd2bf6fa126b699641d9e15c2dfdf124f4e93ea
Vitali <vitali007@tut.by>
2024-04-07 22:27:06 +0300

    create global, start buttons

---
+++ src/Element/Element.tsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Button = styled.button<{ primary?: boolean }>`
+  background: ${props => props.primary ? "#BF4F74" : "white"};
+  color: ${props => props.primary ? "white" : "#BF4F74"};
+
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid #BF4F74;
+  border-radius: 3px;
+  opacity: .8;
+
+    &:hover {
+    opacity: 1;
+  }
+`
+
+export const TomatoButton = styled(Button)`
+  background: tomato;
+  order-color: tomato;
+`
